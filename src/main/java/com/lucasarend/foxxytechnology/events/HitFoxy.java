f17101286aa2393f55bcd48c17892f35c08dbb77
Lucas Arend <lucasarend@night-fox.net>
2023-02-12 02:13:31 -0300

    HitFox
    - Add new Entity tips

--- src/main/java/com/lucasarend/foxxytechnology/events/HitFoxy.java
+++ src/main/java/com/lucasarend/foxxytechnology/events/HitFoxy.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.monster.CaveSpider;
 import net.minecraft.world.entity.monster.Creeper;
+import net.minecraft.world.entity.monster.Pillager;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -17,19 +18,32 @@ import org.slf4j.Logger;
 public final class HitFoxy {
     @SubscribeEvent
     public static void onPlayerAttacked(AttackEntityEvent e) {
+
+        long worldTime = e.getEntity().getLevel().dayTime();
+
         if (e.getTarget().getType() == EntityType.FOX) {
             FoxxyTechnology.LOGGER.info("Não bata em uma raposa!");
-            //Make a Spawn Cave Spider
-            Entity entityToSpawn = new CaveSpider(EntityType.CAVE_SPIDER, e.getTarget().getLevel());
-            Entity entityToSpawn2 = new Creeper(EntityType.CREEPER, e.getTarget().getLevel());
+
             //Get Fox Position
             Vec3 positionSpawn = e.getTarget().position();
             //Add 1 in y
             positionSpawn.add(0,1,0);
-            //Set Position to Spawn Cave Spider
+
+            Entity entityToSpawn = null;
+            Entity entityToSpawn2 = null;
+
+            if ((worldTime > 13000) || (worldTime < 1000)) {
+                entityToSpawn = new CaveSpider(EntityType.CAVE_SPIDER, e.getTarget().getLevel());
+                entityToSpawn2 = new Creeper(EntityType.CREEPER, e.getTarget().getLevel());
+            } else {
+                entityToSpawn = new Pillager(EntityType.PILLAGER, e.getTarget().getLevel());
+                entityToSpawn2 = new Creeper(EntityType.CREEPER, e.getTarget().getLevel());
+            }
+
             entityToSpawn.setPos( positionSpawn );
             entityToSpawn2.setPos( positionSpawn );
-            //Spawn Cave Spider
+
+            //Spawn Mobs
             e.getTarget().getLevel().addFreshEntity(entityToSpawn);
             e.getTarget().getLevel().addFreshEntity(entityToSpawn2);
         }

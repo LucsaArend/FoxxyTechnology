f0b9097997ea0e49e755c503818e3741c37da09d
Lucas Arend <lucasarend@night-fox.net>
2023-04-27 16:33:37 -0300

    Space

--- src/main/java/com/lucasarend/foxxytechnology/itens/ModItems.java
+++ src/main/java/com/lucasarend/foxxytechnology/itens/ModItems.java
@@ -16,19 +16,14 @@ public class ModItems {
     //Itens
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FoxxyTechnology.MODID);
-
     public static final RegistryObject<Item> BASICFOXYINGOT = ITEMS.register("basicfoxyingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOXXYTECHNOLOGY_TAB)));
-
     public static final RegistryObject<Item> ADVANCEDFOXYINGOT = ITEMS.register("advancedfoxyingot",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOXXYTECHNOLOGY_TAB)));
-
     public static final RegistryObject<Item> FOXYGEM = ITEMS.register("foxygem",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOXXYTECHNOLOGY_TAB)));
-
     public static final RegistryObject<Item> MERCURY = ITEMS.register("mercury",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOXXYTECHNOLOGY_TAB)));
-
     //Upgrade Itens
     public static final RegistryObject<Item> FOXY_UPGRADE_RAGE = ITEMS.register("foxyupgraderage",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.FOXXYTECHNOLOGY_TAB)));
@@ -37,17 +32,12 @@ public class ModItems {
     //Tools
     public static final RegistryObject<Item> FOXY_BASIC_PICKAXE = ITEMS.register("foxybasicpickaxe",
             () -> new BasicFoxyPickaxe());
-
     public static final RegistryObject<Item> FOXY_BASIC_AXE = ITEMS.register("foxybasicaxe",
             () -> new BasicFoxyAxe());
-
     public static final RegistryObject<Item> FOXY_BASIC_SHOVEL = ITEMS.register("foxybasicshovel",
             () -> new BasicFoxyShovel());
-
     public static final RegistryObject<Item> FOXY_BASIC_SWORD = ITEMS.register("foxybasicsword",
             () -> new BasicFoxySword());
-
-
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
